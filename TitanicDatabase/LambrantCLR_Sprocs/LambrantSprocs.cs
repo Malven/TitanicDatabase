ae7f1a13a5aa1bdb4f27cbd7bc1663d1f7c83f0c
andreas lambrant <andreas.lambrant@live.se>
2016-03-24 13:25:49 +0100

    Added a new proj for my CLR Sprocs, added a new class to contain them. Started with the first method which allows a user to search the database based on age.

---
+++ TitanicDatabase/LambrantCLR_Sprocs/LambrantSprocs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+
+public class LambrantSprocs
+{
+    [Microsoft.SqlServer.Server.SqlProcedure]
+    public static SqlInt32 SearchByAge(SqlInt32 age)
+    {
+        using (SqlConnection conn = new SqlConnection("context connection=true"))
+        {
+            SqlCommand comm = new SqlCommand();
+
+            if (age < 0 || age > 120)
+                return 0;
+
+            comm.CommandText = "SELECT p.Lastname, p.Firstname, p.Age" + 
+                                "FROM Passenger AS p" +
+                                "WHERE p.Age < " + age + ";" +
+                                "SELECT cr.Lastname, cr.Firstname, cr.Age" +
+                                "FROM Crew AS cr" + 
+                                "WHERE cr.Age < " + age + ";";
+
+            comm.Connection = conn;
+            conn.Open();
+            comm.ExecuteNonQuery();
+            conn.Close();
+            return 1;
+        }
+    }
+
+}
