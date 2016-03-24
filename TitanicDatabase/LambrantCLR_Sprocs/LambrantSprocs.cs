ccecbdc4a8b26b0fe0ad5efbab1bc53435e0e65d
andreas lambrant <andreas.lambrant@live.se>
2016-03-24 16:54:41 +0100

    Done with SearchByAge procedure

--- TitanicDatabase/LambrantCLR_Sprocs/LambrantSprocs.cs
+++ TitanicDatabase/LambrantCLR_Sprocs/LambrantSprocs.cs
@@ -7,10 +7,22 @@ using System.Data.SqlClient;
 using System.Data.SqlTypes;
 using Microsoft.SqlServer.Server;
 
+/*
+
+- Search amongst all the passengers and crewmembers by age.
+
+- Check what kind of cabin the relative lived in based on lastname.
+
+- If their relative was a member of the crew they should be able to check which department he/she worked for.
+
+- If their relative was a member of the crew they should be able to check which class they took care of, 1st-, 2nd- or 3rd class.
+
+*/
+
 public class LambrantSprocs
 {
     [Microsoft.SqlServer.Server.SqlProcedure]
-    public static SqlInt32 SearchByAge(SqlString age)
+    public static void SearchByAge(SqlString age)
     {
         using (SqlConnection conn = new SqlConnection("context connection=true"))
         {
@@ -22,19 +34,44 @@ public class LambrantSprocs
             //if (age < "0" || age > "120" || age == "" || !isNum)
             //    return 0;
 
-            comm.CommandText = "SELECT p.Lastname, p.Firstname, p.Age" +
-                                "FROM Passenger AS p" +
-                                "WHERE p.Age < " + age.ToString() + ";" +
-                                "SELECT cr.Lastname, cr.Firstname, cr.Age" +
-                                "FROM Crew AS cr" + 
-                                "WHERE cr.Age < " + age.ToString() + ";";
+            comm.CommandText = "SELECT Lastname, Firstname, Age " +
+                                "FROM Passenger " +
+                                "WHERE Age = " + age.ToString() + ";" +
+                                "SELECT Lastname, Firstname, Age " +
+                                "FROM Crew " +
+                                "WHERE Age = " + age.ToString() + ";";
 
             comm.Connection = conn;
             conn.Open();
             //comm.ExecuteNonQuery();
             SqlContext.Pipe.ExecuteAndSend(comm);
             conn.Close();
-            return 1;
+            conn.Dispose();
+            comm.Dispose();
+        }
+    }
+
+    [Microsoft.SqlServer.Server.SqlProcedure]
+    public static void CabinPassenger(SqlString name)
+    {
+        using (SqlConnection conn = new SqlConnection("context connection=true"))
+        {
+            SqlCommand comm = new SqlCommand();
+            
+
+            comm.CommandText = "SELECT p.Lastname, p.Firstname, cab.CabinDescription, " +
+                                "cab.CabinPrice " + 
+                                "FROM Passenger AS p " +
+                                "INNER JOIN Cabin AS cab ON cab.CabinID = p.CabinID " +
+                                "WHERE p.Lastname = " + name.ToString() + " ;";
+
+            comm.Connection = conn;
+            conn.Open();
+            //comm.ExecuteNonQuery();
+            SqlContext.Pipe.ExecuteAndSend(comm);
+            conn.Close();
+            conn.Dispose();
+            comm.Dispose();
         }
     }
 }
